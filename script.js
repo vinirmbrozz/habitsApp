3e5fcce64b50644160f3379e9bc75af729d2b40c
Vinicius <mbrozzgamer@hotmail.com>
2023-01-20 23:11:57 -0300

    fixed error .js

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-const form = document.querySelector('#form-habits')
+const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
-const button = document.querySelector('header button')
+const button = document.querySelector("header button")
 
 button.addEventListener("click", add)
 form.addEventListener("change", save)
 
 function add() {
-    const hoje = new Date().toLocaleDateString('pt-br').slice(0, -5)
-    const diaExiste = nlwSetup.dayExists(hoje)
+  const hoje = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const diaExiste = nlwSetup.dayExists(hoje)
 
-    if(diaExiste){
-        alert("Dia já existente no painel ❌")
-        return
-    }
+  if (diaExiste) {
+    alert("Dia já existente no painel ❌")
+    return
+  }
 
-    alert("Dia adicionado com sucesso ✅")
-    nlwSetup.addDay(hoje)
+  alert("Dia adicionado com sucesso ✅")
+  nlwSetup.addDay(hoje)
 }
 
-function save(){
-    localStorage.setItem('NLWSetup@Habits', JSON.stringify(nlwSetup.data))
+function save() {
+  localStorage.setItem("NLWSetup@Habits", JSON.stringify(nlwSetup.data))
 }
 
-
-const data = JSON.parse(localStorage.getItem('NLWSetup@Habits')) || {}
+const data = JSON.parse(localStorage.getItem("NLWSetup@Habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
-
